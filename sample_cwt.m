19699aae3393f287bae6e3e0301c44bdf2386431
Lev Maximov <lev.maximov@gmail.com>
2010-01-27 02:01:07 +0600

    cwt sample: three bumps

--- sample_cwt.m
+++ sample_cwt.m
@@ -1,8 +1,27 @@
 function sample_cwt()
-x=zeros(1000,1);
-N = 200;
+x=zeros(1,1000);
+
+K=500;
+N=200;
+h=0.1;
+x(K-N:K+N)=x(K-N:K+N)+...
+ sin(0:h:h*2*N).*exp(-(-N:N).^2/8000);
+
+K=400;
+N=100;
+h=0.2;
+x(K-N:K+N)=x(K-N:K+N)+...
+  sin(0:h:h*2*N).*exp(-(-N:N).^2/8000);
+
+K=600;
+N=100;
+h=0.3;
+x(K-N:K+N)=x(K-N:K+N)+...
+  sin(0:h:h*2*N).*exp(-(-N:N).^2/8000);
+
+K=500;
+N=200;
 w1=0.1; w2=0.1; w3=100;
-x(500-N:500+N)=sin(0:0.1:0.1*2*N).*exp(-(-N:N).^2/8000);
-[t,tt,z]=mycwt(x(500-N:500+N),1,w1,w2,w3,'mymorlet');
+[t,tt,z]=mycwt(x(K-N:K+N),1,w1,w2,w3,'mymorlet',1);
 figure(2);
 plot((w1:w2:w3),abs(z(:,N)),'.-');
